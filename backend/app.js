eb161c3dccd28cf09883b0434c4b76ab45496f8d
Michael Bentley <michaelbentley95@gmail.com>
2022-08-31 16:09:15 -0400

    Build out actual CRUD connection

--- backend/app.js
+++ backend/app.js
@@ -11,19 +11,38 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: "Hair dresser",
-        image: "some_url",
-    };
-    res.send(product);
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: { type: Number, required: true },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find();
+
+    if (!productList) {
+        res.status(500).json({ success: false });
+    }
+
+    res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock,
+    });
+    product
+        .save()
+        .then((createdProduct) => {
+            res.status(201).json(createdProduct);
+        })
+        .catch((err) => {
+            res.status(500).json({ error: err, success: false });
+        });
 });
 
 mongoose
